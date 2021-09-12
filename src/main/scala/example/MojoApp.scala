d0fbeac338d9abe3d913ab761eafc1fa10358478
Rikus de Milander <rdmilander@sportingindex.com>
2021-09-12 16:00:59 +0200

    master: Busy with binding of calculations

--- src/main/scala/example/MojoApp.scala
+++ src/main/scala/example/MojoApp.scala
@@ -1,5 +1,6 @@
 package example
 
+import com.github
 import com.github.nscala_time
 import com.github.nscala_time.time
 import com.github.nscala_time.time.Imports
@@ -39,6 +40,11 @@ case class Temperature(temp: DeciDegreeCelsius) extends SensorValue {
   override val timeStamp: Imports.DateTime = DateTime.now()
   private val degreesCelsius: Float = temp.degrees
 }
+object Temperature {
+  def apply(deciDegrees: Int) = {
+    new Temperature(DeciDegreeCelsius(deciDegrees))
+  }
+}
 
 case class Grams(grams: Float)
 case class MilliGrams(milli: Int)
@@ -51,7 +57,51 @@ case class ContainerWeight(weight: MilliGrams) extends SensorValue {
   override val timeStamp: time.Imports.DateTime = DateTime.now()
   private val grams: Float = weight.grams
 }
+object ContainerWeight {
+  def apply(milliGrams: Int): ContainerWeight = {
+    new ContainerWeight(MilliGrams(milliGrams))
+  }
+}
 
 case class Mojo() extends SensorValue {
   override val timeStamp: nscala_time.time.Imports.DateTime = DateTime.now()
+}
+
+case class CalculationValue[A](value: A, timeStamp: DateTime) {
+  def map[B](f: A => B): CalculationValue[B] =
+    CalculationValue(f(value), DateTime.now())
+  def flatMap[B](f: A => CalculationValue[B]): CalculationValue[B] = {
+    f(value)
+  }
+}
+
+object Calculations {
+
+  def oldestTimestamp(
+      timeStamps: List[DateTime]
+  ): Option[github.nscala_time.time.Imports.DateTime] = {
+    timeStamps.sorted.headOption
+  }
+
+  def avgWeightPerSample(
+      containerWeight: ContainerWeight
+  )(numberOfSamples: NumberOfSamples): CalculationValue[Grams] =
+    CalculationValue[Grams](
+      Grams(containerWeight.weight.grams / numberOfSamples.number),
+      DateTime.now()
+    )
+
+  def calculate[A, B](
+      sensorValue: SensorValue
+  )(calculationValue: CalculationValue[A])(
+      f: SensorValue => CalculationValue[A] => CalculationValue[B]
+  ): CalculationValue[B] = {
+    val newCalculation = f(sensorValue)(calculationValue)
+    //oldest timestamp
+    CalculationValue(
+      newCalculation.value,
+      oldestTimestamp(sensorValue.timeStamp :: newCalculation.timeStamp :: Nil)
+        .getOrElse(DateTime.now())
+    )
+  }
 }
--- src/test/scala/example/MojoSpec.scala
+++ src/test/scala/example/MojoSpec.scala
@@ -2,9 +2,32 @@ package example
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import Calculations._
 
 class MojoSpec extends AnyFlatSpec with Matchers {
   "The Mojo object" should "get the mojo" in {
-    //todo
+
+    val containerWeight: ContainerWeight = ContainerWeight(1000)
+    val numberOfSamples: NumberOfSamples = NumberOfSamples(6)
+    val temperature: Temperature = Temperature(10)
+
+    for {
+      w <- avgWeightPerSample(containerWeight)(numberOfSamples)
+      r2 <- calculate(temperature)(w)
+    } yield {}
+
+    C1(s1, s2).flatMap { r1 =>
+      C2(s3, r1).flatMap { r2 => }
+    }
+
+    for {
+      r1 <- C1(s1, s2) //avgWeightPerSample
+      r2 <- C2(s3, r1)
+    } yield {}
+
+    C1(s1, s2).flatMap { r1 =>
+      C2(s3, r1).flatMap { r2 => }
+    }
+
   }
 }
